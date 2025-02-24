21ecf1c6f231109a87e736bc75a5ec3ecef5e01b
jnrchristian <110577327+jnrchristian@users.noreply.github.com>
2025-02-24 08:15:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ document.querySelector("form").addEventListener("submit", function (e) {
     alert("Thank you for your message! We will get back to you soon.");
   });
   
-  const hamburger = document.getElementById('hamburger-icon');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle the 'active' class on the nav links when the hamburger is clicked
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
