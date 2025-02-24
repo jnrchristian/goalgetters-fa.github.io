fd03c9aa50e44d94d1a5b5252a43e4123f57a7cc
jnrchristian <110577327+jnrchristian@users.noreply.github.com>
2025-02-24 16:47:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Thank you for your message! We will get back to you soon.");
   });
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');  // Toggle the active class to show or hide the menu
+});
